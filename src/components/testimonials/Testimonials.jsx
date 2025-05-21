8ac8538922eed7bc758dbb6d6b0e491f66977cfa
NunoF <n.fernandes.contact@gmail.com>
2025-05-22 01:00:50 +0200

    feat(testimonials): add testimonials component with data and styling
    Introduce a new testimonials section to the application. This includes:
    - Adding avatar images for testimonials
    - Creating a CSS file for styling the testimonials layout and content
    - Implementing the Testimonials component with dynamic data rendering
    - Adding a testimonials data file with sample entries for display

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,10 +8,30 @@
  */
 import "./Testimonials.css";
 
+/**
+ * data
+ */
+import { testimonials } from "../../utils/Testimonials";
+
 export default function Testimonials() {
   return (
     <section id="testimonials" className="section__size section container">
       <h2 className="section__title">Testimonials</h2>
+
+      <div className="testimonials__container">
+        {testimonials.map(({ id, avatar, title, subtitle, comment }) => (
+          <div key={id} className="testimonials__content">
+            <div className="testimonial__author">
+              <img src={avatar} className="w-20 h-20 rounded-full" />
+              <div>
+                <h3>{title}</h3>
+                <span className="testomonial__subtitle">{subtitle}</span>
+              </div>
+            </div>
+            <p className="comment">{`" ${comment} "`}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/utils/testimonials.js
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    id: 1,
+    avatar: "/images/avatar/girl.png",
+    title: "Barbara Gracoeiro",
+    subtitle: "Chargé de recouvrement",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum, voluptas cumque, adipisci, consequuntur accusamus voluptatibus temporibus voluptate inventore reprehenderit necessitatibus.",
+  },
+  {
+    id: 2,
+    avatar: "/images/avatar/men.png",
+    title: "Pedro Gonçalves",
+    subtitle: "Directeur technique",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum, voluptas cumque, adipisci, consequuntur accusamus voluptatibus temporibus voluptate inventore reprehenderit necessitatibus.",
+  },
+  {
+    id: 3,
+    avatar: "/images/avatar/men.png",
+    title: "Lorem ipsum",
+    subtitle: "consectetur",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum, voluptas cumque, adipisci, consequuntur accusamus voluptatibus temporibus voluptate inventore reprehenderit necessitatibus.",
+  },
+];
