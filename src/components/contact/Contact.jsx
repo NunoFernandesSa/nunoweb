f47e7e81e8aff91303b91d4a75b7a7c5e0252e02
NunoF <n.fernandes.contact@gmail.com>
2025-05-22 14:38:34 +0200

    style(contact): adjust spacing and add hover effects to links
    - Added `mb-8 md:mb-0` to the contact section for better spacing on mobile and desktop
    - Added hover effects with transition to email and phone number links for improved user interaction

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
       <h2 className="section__title">Contact</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="flex flex-col justify-start items-start gap-3">
+        <div className="flex flex-col justify-start items-start gap-3 mb-8 md:mb-0">
           <div className="">
             <h3 className="text-h2FontSize">Get in touch</h3>
             <p className="text-secondary">
@@ -28,10 +28,18 @@ export default function Contact() {
           </div>
 
           <div className="flex flex-col justify-center items-start gap-2 italic text-primary">
-            <a href="mailto::n.fernandes.contact@gmail.com" className="">
+            <a
+              href="mailto::n.fernandes.contact@gmail.com"
+              className="hover:text-accent transition-all ease-in-out duration-300"
+            >
               n.fernandes.contact@gmail.com
             </a>
-            <a href="">+33 7 71 80 29 21</a>
+            <a
+              href="tel:+33771802921"
+              className="hover:text-accent transition-all ease-in-out duration-300"
+            >
+              +33 7 71 80 29 21
+            </a>
           </div>
           <img
             src="/images/logo-white.png"
