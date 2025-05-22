629bd7bc170b02312d3f7e747194b1dfd5fc2772
NunoF <n.fernandes.contact@gmail.com>
2025-05-22 14:18:35 +0200

    refactor(contact): extract contact form into separate component
    Extract the contact form logic and UI from the Contact component into a dedicated ContactForm component to improve code modularity and maintainability. This change also enhances the readability of the Contact component by reducing its complexity.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,14 +4,9 @@
  */
 
 /**
- * formik & yup
+ * components
  */
-import { useFormik } from "formik";
-
-/**
- * Validators
- */
-import { validationSchema } from "../../validators/validatorContactForm";
+import ContactForm from "./contactForm/ContactForm";
 
 /**
  * Style css
@@ -19,102 +14,34 @@ import { validationSchema } from "../../validators/validatorContactForm";
 import "./Contact.css";
 
 export default function Contact() {
-  // Initialisation de Formik
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      message: "",
-    },
-    validationSchema,
-    onSubmit: (values, { setSubmitting, resetForm }) => {
-      // Ici vous pouvez ajouter votre logique d'envoi
-      console.log("Données du formulaire:", values);
-
-      // Simulation d'envoi asynchrone
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-        resetForm();
-      }, 1000);
-    },
-  });
-
   return (
     <section id="contact" className="section__size section container">
       <h2 className="section__title">Contact</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="">Get in touch</div>
-        <div className="">
-          <form onSubmit={formik.handleSubmit} className="contact-form">
-            <div className="form-group">
-              <input
-                id="name"
-                name="name"
-                type="text"
-                placeholder="John Doe"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-                className={
-                  formik.touched.name && formik.errors.name ? "error" : ""
-                }
-              />
-              {formik.touched.name && formik.errors.name ? (
-                <div className="error-message">{formik.errors.name}</div>
-              ) : null}
-            </div>
+        <div className="flex flex-col justify-start items-start gap-3">
+          <div className="">
+            <h3 className="text-h2FontSize">Get in touch</h3>
+            <p className="text-secondary">
+              Don&apos;t like forms? Send me an email.
+            </p>
+          </div>
 
-            <div className="form-group">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="doe@gmail.com"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-                className={
-                  formik.touched.email && formik.errors.email ? "error" : ""
-                }
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <div className="error-message">{formik.errors.email}</div>
-              ) : null}
-            </div>
-
-            <div className="form-group">
-              <textarea
-                id="message"
-                name="message"
-                rows="5"
-                placeholder="Hello...!"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.message}
-                className={
-                  formik.touched.message && formik.errors.message ? "error" : ""
-                }
-              />
-              {formik.touched.message && formik.errors.message ? (
-                <div className="error-message">{formik.errors.message}</div>
-              ) : null}
-            </div>
-
-            <button
-              type="submit"
-              disabled={formik.isSubmitting || !formik.isValid || !formik.dirty}
-              className={`submit-btn ${
-                formik.isSubmitting || !formik.isValid || !formik.dirty
-                  ? "opacity-50 cursor-not-allowed"
-                  : ""
-              }`}
-            >
-              {formik.isSubmitting ? "Sending..." : "Send message"}
-            </button>
-          </form>
+          <div className="flex flex-col justify-center items-start gap-2 italic text-primary">
+            <a href="mailto::n.fernandes.contact@gmail.com" className="">
+              n.fernandes.contact@gmail.com
+            </a>
+            <a href="">+33 7 71 80 29 21</a>
+          </div>
+          <img
+            src="/images/logo-white.png"
+            alt=""
+            width={100}
+            className="bg-accent rounded-lg"
+          />
         </div>
+
+        <ContactForm />
       </div>
     </section>
   );
