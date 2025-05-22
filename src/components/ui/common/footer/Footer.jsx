49e0b4029042421e76dae7ad912c1aaa30efc950
NunoF <n.fernandes.contact@gmail.com>
2025-05-22 14:00:49 +0200

    style(Footer): add margin-top to footer component
    Add `mt-16` to the footer's className to ensure proper spacing between the content and the footer, improving the overall layout consistency.

--- src/components/ui/common/footer/Footer.jsx
+++ src/components/ui/common/footer/Footer.jsx
@@ -10,7 +10,7 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="container h-16 flex flex-wrap justify-between items-center px-3 md:px-20 text-xs mb-20 md:mb-0">
+    <footer className="container h-16 flex flex-wrap justify-between items-center px-3 md:px-20 text-xs mb-20 md:mb-0 mt-16">
       <div className="w-full md:w-auto">&copy; 2025 nunoweb</div>
       <div className="w-full md:w-auto"></div>
       <a href="" className="hover:text-hover">
